475f25b25256e59d911cd8b3c1f5e9f85a572e68
JamilServices <jamilservicos@gmail.com>
2024-01-01 11:13:57 -0300

    feat: Add autoLoaderNameSpace function to main.js
    Added the autoLoaderNameSpace function to main.js to provide a convenient way to initialize NameSpace with configurable options. The function creates instances of the NameSpace class and assigns them to a global variable if enableGlobal is true. Otherwise, the instances are stored in the instances object.

--- src/main.js
+++ src/main.js
@@ -1,4 +1,24 @@
 "use strict";
 
+const {NameSpace} = require("./mods/nameSpace");
 
-exports = module.exports = {};
+let instances = {};
+
+const autoLoaderNameSpace = (config = {}) => {
+    try {
+        let prefixGlobal = 'ns';
+        let enableGlobal = true;
+
+        const {mut, prefix, isolate} = config;
+        if(isolate) enableGlobal = false;
+        if (typeof prefix === "string") prefixGlobal = prefix.toString();
+        if (typeof instances[prefixGlobal] === "undefined") instances[prefixGlobal] = new NameSpace({mut, prefix: prefixGlobal});
+        if (enableGlobal) global[prefixGlobal] = instances[prefixGlobal];
+        return instances[prefixGlobal];
+    } catch {
+        console.error("Error on init NameSpace");
+    }
+    return {};
+};
+
+exports = module.exports = autoLoaderNameSpace;
