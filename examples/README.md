2f1f5235453d6f25ba140090b6383e5bfc92a119
JamilServices <jamilservicos@gmail.com>
2024-01-01 08:30:58 -0300

    docs(example): update README.md

--- examples/README.md
+++ examples/README.md
@@ -1,1 +1,1 @@
-### Example of how to access and add new elements, through several files through the global scope.
+### Examples
--- examples/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-require("..")();
-ns.deps("typesHelper", require("@jamilservices/types-helper"));
-ns.deps("example", require("./test"));
-const test1 = require("./test1");
-console.log(ns.storage("end"));
