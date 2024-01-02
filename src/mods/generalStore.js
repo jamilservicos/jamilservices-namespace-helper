e910cfb96f7f1a315688fefd71a8884e17076284
JamilServices <jamilservicos@gmail.com>
2024-01-02 06:19:27 -0300

    feat(generalStore): add general store management functions
    Added functions for managing the general store. This includes retrieving the content for a specific prefix, changing a specific value in the store, setting a key with a specific value, and retrieving the value for a specific key. These functions are intended for internal use within the application or package.
    
    Changes:
    - Added showGeneralStore function
    - Added changeValue function
    - Added setGeneralStoreKey function
    - Added getGeneralValue function

--- src/mods/generalStore.js
+++ src/mods/generalStore.js
@@ -1,7 +1,23 @@
 "use strict";
 
+/**
+ * Stores general settings or values.
+ * @private
+ * @type {Object}
+ * @memberof module:GeneralStoreModule
+ */
 const generalStore = {};
 
+/**
+ * Retrieves the content for a specific prefix from the general store.
+ *
+ * This function returns the object associated with the given prefix in the generalStore if it exists and is not empty.
+ *
+ * @function showGeneralStore
+ * @memberof module:GeneralStoreModule
+ * @param {string} prefix - The prefix key for the content to retrieve.
+ * @returns {Object} - The object associated with the prefix, or an empty object if not found or on error.
+ */
 const showGeneralStore = (prefix) => {
     try {
         if (generalStore[prefix] && Object.keys(generalStore[prefix]).length > 0) return {...generalStore[prefix]};
@@ -11,12 +27,40 @@ const showGeneralStore = (prefix) => {
     return {};
 };
 
+/**
+ * Changes a specific value in the general store.
+ *
+ * This internal function updates the value of a specific key within the generalStore.
+ *
+ * @function changeValue
+ * @memberof module:GeneralStoreModule
+ * @param {Object} data - The object containing the prefix, key, and new value to set.
+ * @param {string} data.prefix - The prefix key for the value to update.
+ * @param {string} data.key - The key within the store to update.
+ * @param {*} data.value - The new value to set for the key.
+ * @returns {boolean} - True if the operation was successful, false otherwise.
+ */
 const changeValue = (data) => {
     const {prefix, key, value} = data;
     generalStore[prefix][key.toString()] = value;
     return true;
 };
 
+/**
+ * Sets a key with a specific value in the general store.
+ *
+ * This function sets a key-value pair within the generalStore for a given prefix.
+ * It can enforce immutability based on the 'mut' flag.
+ *
+ * @function setGeneralStoreKey
+ * @memberof module:GeneralStoreModule
+ * @param {Object} data - The object containing the key, value, and other options for setting the content.
+ * @param {string} data.key - The key to set in the store.
+ * @param {*} data.value - The value to set for the key.
+ * @param {boolean} [data.mut=false] - Determines if the key can be mutated in the future.
+ * @param {string} data.prefix - The prefix key for the store to update.
+ * @returns {boolean} - True if the key was set successfully, false otherwise.
+ */
 const setGeneralStoreKey = (data) => {
     try {
         const {key, value, mut, prefix} = data;
@@ -33,6 +77,18 @@ const setGeneralStoreKey = (data) => {
     return false;
 };
 
+/**
+ * Retrieves the value for a specific key from the general store.
+ *
+ * This function returns the value associated with a given key in the generalStore, if it exists.
+ *
+ * @function getGeneralValue
+ * @memberof module:GeneralStoreModule
+ * @param {Object} data - The object containing the prefix and key to retrieve the value for.
+ * @param {string} data.key - The key to retrieve the value for.
+ * @param {string} data.prefix - The prefix key for the store to retrieve from.
+ * @returns {*} - The value associated with the key, or undefined if not found or on error.
+ */
 const getGeneralValue = (data) => {
     try {
         const {key, prefix} = data;
@@ -46,6 +102,13 @@ const getGeneralValue = (data) => {
     return undefined;
 };
 
+/**
+ * Exports the general store management functions.
+ * These functions are intended for internal use within the application or package.
+ *
+ * @private
+ * @module GeneralStoreModule
+ */
 exports = module.exports = {
     ...{showGeneralStore},
     ...{setGeneralStoreKey},
